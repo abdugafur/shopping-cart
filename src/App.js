21a2658092c6c24b7387e1b54af007cb41a1cc74
abdugafur <abdugafur3323@gmail.com>
2021-07-30 15:42:24 +0500

    Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Cart from "./Components/Cart";
+import Nav from "./Components/Nav";
+import HomeScreen from "./screens/HomeScreen";
+
+function App() {
+  return (
+    <div className='App'>
+      <Nav />
+      <Cart />
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
